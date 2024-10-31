31a6d3eddfdda99108c3ff87ff3824741e5bc90a
Albraaibrahim8 <96339339+Albraaibrahim8@users.noreply.github.com>
2024-10-31 12:55:28 +0200

    Update C_Equalize.cpp

--- C_Equalize.cpp
+++ C_Equalize.cpp
@@ -1,12 +1,22 @@
+//https://codeforces.com/problemset/problem/1037/C
+#include<bits/stdc++.h>
+using namespace std;
 
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-        cout << min_changes(s) << "\n";
+int main(){
+    int n; cin>>n;
+    string s1,s2; cin>>s1>>s2;
+    int cnt=0;
+    int i=0;
+    for(;i<n-1;i++){
+        if(s1[i]==s2[i])continue;
+        if((s1[i]=='1' and s1[i+1]=='0')and(s2[i]=='0' and s2[i+1]=='1')){
+            cnt++; 
+            i++;
+        }else if((s1[i]=='0' and s1[i+1]=='1')and(s2[i]=='1' and s2[i+1]=='0')){
+            cnt++; 
+            i++;
+        }else cnt++;
     }
-    return 0;
+    if(i==n-1 and s1[i]!=s2[i])cnt++;
+    cout<<cnt<<"\n";
 }
